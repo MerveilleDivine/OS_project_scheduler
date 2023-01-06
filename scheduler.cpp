bba6ed2cfe6cd81a9d9e2bdae5cffb3dccd4cf04
vinemuganguzi <mervinemuganguao@gmail.com>
2023-01-06 18:05:18 +0200

    safer uploading file modification of cmd line implementation code. I added a catch try to avoid any crash in the program. well done!

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,32 +1,60 @@
+
 #include <iostream>
+#include <fstream>
 #include <cstring>
+#include <stdexcept>
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    // Initialize variables for input and output file names
-    char* input_file = NULL;
-    char* output_file = NULL;
+//my_argc is the argument count and my_argv is the argument vector
+int main(int my_argc, char** my_argv) {
+  // Initialize variables for input and output file names
+  string input_file;
+  string output_file;
 
-    // Parse the command line arguments
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-f") == 0) {
-            input_file = argv[i+1];
-        } else if (strcmp(argv[i], "-o") == 0) {
-            output_file = argv[i+1];
-        }
+  // Parse the command line arguments
+  for (int i = 1; i < my_argc; i++) {
+    if (strcmp(my_argv[i], "-f") == 0) {
+      input_file = my_argv[i+1];
+    } else if (strcmp(my_argv[i], "-o") == 0) {
+      output_file = my_argv[i+1];
+    }
+  }
+
+  try {
+    // Open the input file
+    ifstream input(input_file);
+    if (!input.is_open()) {
+      throw runtime_error("Failed to open input file: " + input_file);
+    }
+
+    // Open the output file
+    ofstream output(output_file);
+    if (!output.is_open()) {
+      throw runtime_error("Failed to open output file: " + output_file);
     }
 
     // Print the input and output file names
     cout << "Input file: " << input_file << endl;
     cout << "Output file: " << output_file << endl;
 
-    return 0;
-}
+    // Read from the input file and write to the output file
+    string line;
+    while (getline(input, line)) {
+      output << line << endl;
+    }
+  } catch (const exception& e) {
+    // Exception handling code goes here
+    cerr << "Error: " << e.what() << endl;
+    return 1;
+  }
 
+  return 0;
 
-/* to run this part in cmd line
-  ./filename -f input.txt -o output.txt
+  /* to implement file from cmd line use
+  ./nameOfTheFile.extension -f input.txt -o output.txt
   don't forget to change the filename
 */
+}
+
 
